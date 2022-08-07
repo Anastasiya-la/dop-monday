43e120acf75b2bbca5bfbb2e498c40b3059ec49c
Anastasiya Rabetskaya <nastia1362@gmail.com>
2022-08-07 16:59:29 +0300

    getRequest from API false + button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Button} from "./components/Button";
+
+type GetType = {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [get, setGet] = useState<Array<GetType>>([]);
+
+    const getRequestHandler = () => {
+        setGet([]);
+    }
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setGet(json))
+    }, []);
+
+    return (
+        <div>
+            <Button name={'CleanPage'} callback={getRequestHandler}/>
+            <ul>
+                {get.map((p) => {
+                    return (
+                        <li>
+                            <span>{p.id} - </span>
+                            <span>{p.userId} - </span>
+                            <span>{p.title} - </span>
+                        </li>)
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
